5f929b2b04dc03699ccf184eb52bce8f314614cf
Katrine-Marie <fjordscene@gmail.com>
2018-12-18 12:14:18 +0100

    Moved the email version into separate folder

--- emailVersion/index.php
+++ emailVersion/index.php
@@ -1,7 +1,7 @@
 <?php
 
-  require_once('config.php'); // Database forbindelse
-  require_once('functions.php'); // Funktioner
+  require_once('../config.php'); // Database forbindelse
+  require_once('../functions.php'); // Funktioner
 
   $ip_address = $_SERVER['REMOTE_ADDR']; // Find den besøgendes IP adresse
 
@@ -51,27 +51,27 @@
 <head>
   <meta charset="UTF-8">
   <title>Diving 2000's Service Rating</title>
-  
+
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  
+
   <link rel="icon" href="https://diving2000.dk/trustpilot/Logo_diving_2000-white-border.png" type="image/x-icon"/>
-  
+
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css">
   <link rel="stylesheet" href="style.css">
-  
+
 </head>
 <body>
-  
+
   <div class="panel">
     <div class="logo">
       <img src="Logo_diving_2000.png" alt="">
       <h1>
         Diving 2000's service rating
       </h1>
-      
+
     </div>
-    <?php 
-      echo '<p><i class="rate rate-' . $rate . '"></i>' . $message . '</p>'; 
+    <?php
+      echo '<p><i class="rate rate-' . $rate . '"></i>' . $message . '</p>';
     ?>
   </div>
   <?php
@@ -81,6 +81,6 @@
       echo '</div>';
     }
   ?>
-  
+
 </body>
 </html>
